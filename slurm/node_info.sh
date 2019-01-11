cdf60451766552b52a486f8a68b094c7e61653c3
Andrea Telatin (QIB) <telatina@v0768.nbi.ac.uk>
2019-01-11 16:37:33 +0000

    Polishing demo node_info

--- slurm/node_info.sh
+++ slurm/node_info.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
+# This is a script that will be executed befor the command sent
+# throught the Slurm wrapper (sb.pl / sw.pl)
+
 VER=18.10
 
-echo "== VARS"
-echo "++ JobID:     $SLURM_JOB_ID"
-echo "++ Nodes:     $SLURM_JOB_NODELIST"
-echo "++ Tasks:     $SLURM_NTASKS"
-echo "-- Threads:   $THIS_JOB_CORES"
-echo "-- JobName:   $THIS_JOB_NAME"
-echo "-- LogDir:    $THIS_JOB_LOGDIR"
+echo "# VARS"
+echo "- JobID:        $SLURM_JOB_ID"
+echo "- Nodes:        $SLURM_JOB_NODELIST"
+echo "- Tasks:        $SLURM_NTASKS"
+echo "- sb/Threads:   $THIS_JOB_CORES"
+echo "- sb/JobName:   $THIS_JOB_NAME"
+echo "- sb/LogDir:    $THIS_JOB_LOGDIR"
 echo ""
-echo "== Machine"
-echo -n "-- Hostname:"
+echo "# Machine"
+echo -n "- Hostname:"
 hostname -f
-echo "-- NodeID:$SLURM_NODEID"
-echo "== FREE_MEM_GB"
+echo "- NodeID:$SLURM_NODEID"
+echo "# FREE_MEM_GB"
 free -g
 
-echo "== PATH"
+echo "# PATH"
 echo $PATH | sed 's/:/\n/g'
 
-echo "== PWD"
+echo "# PWD"
 pwd
 
-echo "== DATE"
+echo "# DATE"
 date
 
-echo "== CPUS"
+echo "# CPUS"
 cat /proc/cpuinfo | grep process | wc -l
 
 
-echo "== VERSION"
-echo "-- Node_info_v:$VER"
+echo "# VERSION"
+echo "- Node_info_v:$VER"
